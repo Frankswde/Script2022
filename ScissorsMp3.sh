c90b75d451894aabb6f7a8aa467d3e1a1bf09332
Frankswde <swde123@gmail.com>
2022-04-22 14:11:18 +0800

    2022/04/22 add script

---
+++ ScissorsMp3.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+#Program: 快速分割MP3
+TogMp3=$1
+## 分割mp3
+mp3splt $TogMp3 00.00  04.20
+mp3splt $TogMp3 04.20  08.38
+mp3splt $TogMp3 08.38  12.37
+mp3splt $TogMp3 12.37  15.48
+mp3splt $TogMp3 15.48  19.13
+mp3splt $TogMp3 19.13  23.21
+mp3splt $TogMp3 23.21  28.40
+mp3splt $TogMp3 28.40  33.22
+mp3splt $TogMp3 33.22  38.15
+mp3splt $TogMp3 38.15  43.15
+mp3splt $TogMp3 43.15  48.01
+mp3splt $TogMp3 48.01  52.57
+mp3splt $TogMp3 52.57  56.21
+mp3splt $TogMp3 56.21  61.09
+mp3splt $TogMp3 61.09  65.11
+mp3splt $TogMp3 65.11  70.13
+mp3splt $TogMp3 70.13  74.39
