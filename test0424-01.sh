b15a473389e7b26a778d3cd84289db55a3440008
Frankswde <swde123@gmail.com>
2022-04-24 11:32:00 +0800

    Shell_Script:if/else條件判斷式

--- test0424-01.sh
+++ test0424-01.sh
@@ -4,3 +4,12 @@
 #History:
 # 2022/04/24
 ##+ New add Script
+### Shell Script 的 if / else 條件判斷式會用 test 或者中括號 “[ ]” 表達
+### -n $str 當 $str1 不是 null, 回傳 true.
+### -z $str 當 $str1 是 null, 回傳 true
+if [ -z $1  ]
+then
+    echo "var is Null"
+else
+    echo "var is "$1
+fi
