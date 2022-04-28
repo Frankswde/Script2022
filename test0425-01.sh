b61e1606e29cebcf05780f3bf31eb3007ee54ea0
Frankswde <swde123@gmail.com>
2022-04-28 16:07:25 +0800

    04mothsciptdell

--- test0425-01.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-#Program:
-# 練習書中範例
-#History:
-# 2022/04/25
-##+ New add Script
-### 用指令與``來獲取要監控的本地服務器IP地址
-NIP=`ip addr | grep inet | grep -vE 'inet6|127.0.0.1' | awk '{print $2}'`
-echo "IP地址："$NIP
